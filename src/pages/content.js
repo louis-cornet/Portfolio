05223f1cfed79ab1ba74e0ca8abab2982ba8f364
louis.cornet <louis.cornet@etu.umontpellier.fr>
2024-01-15 17:56:31 +0100

    first commit

---
+++ src/pages/content.js
@@ -0,0 +1,96 @@
+// Personal Information --> #hero section
+export const name = "Louis Cornet"
+export const designation = "Etudiant Informatique à l'IUT de Montpellier"
+export const location = "Montpellier, France"
+export const pronouns = "he/him"
+export const website = "https://www.linkedin.com/in/louis-cornet-8924021b7/"
+
+// About
+export const about =
+	"\n" +
+	"	Actuellement en troisième année de BUT Informatique à Montpellier, j'ai 22 ans et je nourris un vif intérêt pour le domaine de l'informatique ainsi que les métiers du numérique.\n" +
+	"\n" + "\n" +
+	"	En tant qu'enfant d'expatriés, ayant vécu 5 ans aux Émirats arabes unis et 8 ans aux États-Unis, je me considère comme une personne curieuse et ouverte d'esprit. Sur le plan professionnel, je suis reconnu pour ma rigueur et mon assiduité. Mon amour pour l'informatique se manifeste aussi bien dans des projets individuels que dans des collaborations en équipe, où j'apprécie particulièrement contribuer de manière constructive."
+
+// Work Experience --> #work section
+export const work = [
+	{
+		from: "Avril 2023",
+		to: "Aujourd'hui",
+		role: "Alternant développeur back-end C#",
+		org: "INOVEA",
+		org_url: "https://www.inovea-group.com/",
+		location: "Montpellier, 34090",
+		skills: "Développement API C#" + "\n" +
+				"Développement et déploiement de bot Discord" + "\n" +
+				"Travail d'équipe, méthodes agiles"
+	}
+]
+
+// Projects --> #project section
+export const projects = [
+	{
+		title: "Nuit de l'info a l'IUT 2020 - 2021 :",
+		description: "La nuit de l'info est une période de 16 heures ou plusieurs équipes d'informaticiens et développeurs s'affrontent afin de réaliser des données pas les organisateurs.",
+		tech: "Conduire un projet sous pression.\n" +
+				"Programmer sur une longue durée (15h) en java.\n" +
+				"Adaptabilité.",
+	},
+	{
+		title: "Réalisation d'un jeu Tetris 2022 - 2023 :",
+		description: "Durant ma formation j'ai eu l'occasion de participer au développement d'un jeu retro (Tetris) tout en java et en utilisant la méthode SCRUM.",
+		//url: "https://safefamilyquahog.com",
+		tech: "Méthodes agiles." +"\n"+
+				"Programmer en Java Javafx et fxml."
+	},
+	{
+		title: "Réalisation d’un site web de vente 2021 - 2022 :",
+		description: "Durant ma formation, j'ai eu l'occasion de participer au développement d'un grand nombre d'applications web en php, html, css, et javascript, dont un site web de vente en ligne (non publié).",
+		//url: "https://roadsafetyquahog.com",
+		tech: "Méthodes agiles.\n" +
+			"Adaptabilité."
+	},
+	{
+		title: "Conception d'un site de gestion d'entreprise 2020 - 2021 :",
+		description: "En première année de But informatique, j'ai eu l'occasion d'utiliser Wordpress afin de créer une application web de gestion d'entreprise et de ses employés.",
+		//url: "https://roadsafetyquahog.com",
+		tech: "Réaliser un développement d'application Wordpress." +"\n"+
+		"Conduire un projet.(méthode SCRUM)"
+	}
+]
+
+// Education --> #education section
+export const education = [
+	{
+		from: "2018",
+		to: "2021",
+		institute: "EFB Michelin, Greenville",
+		course: "BAC",
+		location: "Greenville, Caroline du Sud"
+	},
+	{
+		from: "2021",
+		to: "Aujourd'hui",
+		institute: "Institut universitaire de technologie, Montpellier",
+		course: "IAMSI (Intégration d'applications et management des systèmes d'information)",
+		location: "Montpellier, France"
+	},
+]
+
+// Contact --> #contact section
+export const contact = [
+	{
+		source_name: "Email : louis.cornet@etu.umontpellier.fr"
+	},
+	{
+		source_name: "LinkedIn",
+		source: "https://www.linkedin.com/in/louis-cornet-8924021b7/",
+	},
+	{
+		source_name: "CV",
+		source: "https://drive.google.com/file/d/13wIHKtXTGK6XrBXNWB_p4mR6DaRlLofv/view?usp=sharing",
+	},
+]
+
+// Certifications --> #certificate section
+
